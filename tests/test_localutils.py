a0880b38b4db09bb1debb24f133c4cd9f38c370c
httpPrincess <j.rybicki@fz-juelich.de>
2018-12-08 10:39:51 +0100

    towards 50% coverage

--- tests/test_localutils.py
+++ tests/test_localutils.py
@@ -1,6 +1,6 @@
-from airflow_workflows.localutils import get_prefix_from_template
+from airflow_workflows.localutils import get_prefix_from_template, generate_fname
 import datetime
-
+import os
 import unittest
 
 class TestLocalUtils(unittest.TestCase):
@@ -11,6 +11,17 @@ class TestLocalUtils(unittest.TestCase):
     def test_get_prefix_from_template(self):
         pr = get_prefix_from_template(execution_date=datetime.date(2016,12, 24))
         self.assertEqual('test-realtime-gzip/2016-12-24/', pr)
+
+
+    def test_generate_fname(self):
+        ret = generate_fname(suffix='sfx', base_dir='/tmp/', execution_date='2016-12-24')
+        print(ret)
+        self.assertEqual('/tmp/2016-12-24/sfx', ret)
+        self.assertTrue(os.path.isdir('/tmp/2016-12-24/'))
+
+        os.rmdir('/tmp/2016-12-24/')
+        
+
 
 
 if __name__ == "__main__":
