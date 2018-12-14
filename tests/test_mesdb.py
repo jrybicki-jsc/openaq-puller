2fdfa5f92e7223ab36d4a3d56d6f449c0ed05ce2
httpPrincess <j.rybicki@fz-juelich.de>
2018-12-14 12:39:22 +0100

    new testing format

--- tests/test_mesdb.py
+++ tests/test_mesdb.py
@@ -9,6 +9,7 @@ from sqlalchemy.exc import IntegrityError
 from mys3utils.tools import get_jsons_from_stream
 
 import os
+import unittest
 
 jseg = '{"date":{"utc":"2018-06-07T00:00:00.000Z","local":"2018-06-06T20:00:00-04:00"},' \
        '"parameter":"co",' \
@@ -26,212 +27,171 @@ jseg = '{"date":{"utc":"2018-06-07T00:00:00.000Z","local":"2018-06-06T20:00:00-0
        '"mobile":false}'
 
 
-def __get_engine():
-    # host empty is memory storage
-    host = os.getenv('DB_HOST', '')
-    dbname = os.getenv('DB_DATABASE', 'openaq')
-    user = os.getenv('DB_USER', 'jj')
-    password = os.getenv('DB_PASS', 's3cret')
-    engine = MyEngine(host=host, dbname=dbname, user=user, password=password)
+class TestMessDB(unittest.TestCase):
 
-    return engine
+    def setUp(self):
+        # host empty is memory storage
+        host = os.getenv('DB_HOST', '')
+        dbname = os.getenv('DB_DATABASE', 'openaq')
+        user = os.getenv('DB_USER', 'jj')
+        password = os.getenv('DB_PASS', 's3cret')
+        self.engine = MyEngine(host=host, dbname=dbname, user=user, password=password)
 
+        self.smdao = StationMetaCoreDAO(self.engine)
+        self.smdao.create_table()
 
-def test_store_intodb():
-    engine = __get_engine()
-    smdao = StationMetaCoreDAO(engine)
-    smdao.create_table()
+        self.dao = SeriesDAO(self.engine)
+        self.dao.create_table()
 
-    dao = SeriesDAO(engine)
-    dao.create_table()
+        self.mdao = MeasurementDAO(self.engine)
+        self.mdao.create_table()
 
-    mdao = MeasurementDAO(engine)
-    mdao.create_table()
-
-    station, measurement, _ = split_record(json.loads(jseg))
-
-    station_id = smdao.store_from_json(station)
-    res = smdao.get_all()
-    assert len(res) == 1
-
-    series_id = dao.store(station_id=station_id,
-                    parameter=measurement['parameter'],
-                    unit=measurement['unit'],
-                    averagingPeriod="")
-    result = dao.get_all()
-    assert len(result) == 1
-
-    mes_id = mdao.store(series_id=series_id, value=measurement['value'], date=measurement['date']['utc'])
-    assert mes_id == True
-    result = mdao.get_all()
-    assert len(result) == 1
-
-    dao.drop_table()
-    mdao.drop_table()
+    def tearDown(self):
+        self.dao.drop_table()
+        self.mdao.drop_table()
+        self.smdao.drop_table()
 
 
-def test_insert_all():
-    engine = __get_engine()
-
-    smdao = StationMetaCoreDAO(engine)
-    smdao.create_table()
-
-    dao = SeriesDAO(engine)
-    dao.create_table()
-
-    mdao = MeasurementDAO(engine)
-    mdao.create_table()
-
-    station, measurement, _ = split_record(json.loads(jseg))
-    station_id = smdao.store_from_json(station)
-
-    series_id = dao.store(station_id=station_id,
-                    parameter=measurement['parameter'],
-                    unit=measurement['unit'],
-                    averagingPeriod=""
-                    )
-
-    result = dao.get_all()
-    assert len(result) == 1
-
-    mes_id = mdao.store(series_id=series_id, value=measurement['value'], date=measurement['date']['utc'])
-    assert mes_id
-    res = mdao.get_all()
-    assert len(res) == 1
-
-    dao.drop_table()
-    mdao.drop_table()
-
-
-@raises(IntegrityError)
-def test_foreignkey_violation():
-    engine = __get_engine()
-    dao = SeriesDAO(engine)
-    dao.create_table()
-
-    station, measurement, _ = split_record(json.loads(jseg))
-
-    res = dao.store(station_id='non-existing name',
-                    parameter=measurement['parameter'],
-                    unit=measurement['unit'],
-                    averagingPeriod=""
-                    )
-    result = dao.get_all()
-    assert len(result) == 1
-    dao.drop_table()
-
-    if engine.get_engine().name == 'sqlite':
-        raise IntegrityError(statement='SQLLITE', params='does not support', orig='remote key violation',
-                             connection_invalidated=False, code=None)
-
-
-def __get__daos():
-    engine = __get_engine()
-    smdao = StationMetaCoreDAO(engine)
-    smdao.create_table()
-
-    dao = SeriesDAO(engine)
-    dao.create_table()
-
-    mdao = MeasurementDAO(engine)
-    mdao.create_table()
-
-    return smdao, dao, mdao
-
-
-def __clean_daos(smdao, dao, mdao):
-    mdao.drop_table()
-    dao.drop_table()
-    smdao.drop_table()
-
-def test_multiple_inserts():
-    smdao, dao, mdao = __get__daos()
-
-    with open('./tests/series.ndjson', 'rb') as f:
-        ll = list(get_jsons_from_stream(stream=f, object_name='series.ndjson'))
-        assert len(ll) == 15
-
-    for rec in ll:
-        station, measurement, _ = split_record(rec)
-        station_id = smdao.store_from_json(rec)
-        series_id = dao.store(station_id=station_id, 
-            parameter = measurement['parameter'],
-            unit=measurement['unit'],
-            averagingPeriod=f"{measurement['averagingPeriod']}")
+    def test_store_intodb(self):
         
-        mes_id = mdao.store(series_id=series_id, value=measurement['value'], date=measurement['date']['utc'])
-     
-    stations = smdao.get_all()
-    assert len(stations)==4
+        station, measurement, _ = split_record(json.loads(jseg))
 
-    series = dao.get_all()
-    assert len(series) == 6
+        station_id = self.smdao.store_from_json(station)
+        res = self.smdao.get_all()
+        self.assertEqual(len(res), 1)
 
-    mes = mdao.get_all()
-    assert len(mes) == 15
+        series_id = self.dao.store(station_id=station_id,
+                        parameter=measurement['parameter'],
+                        unit=measurement['unit'],
+                        averagingPeriod="")
+        result = self.dao.get_all()
+        self.assertEqual(len(result), 1)
 
-    __clean_daos(smdao, dao, mdao)
-
-
-def test_store_unique_series():
-    smdao, dao, mdao = __get__daos()
-
-    asj = json.loads(jseg)
-    id1 = smdao.store_from_json(asj)
-    assert id1 is not None
-
-    station, measurement, _ = split_record(json.loads(jseg))
-
-    res1 = dao.store(station_id=id1,
-                    parameter=measurement['parameter'],
-                    unit=measurement['unit'],
-                    averagingPeriod=""
-                    )
-
-    
-    res2 = dao.store(station_id=id1,
-                    parameter=measurement['parameter'],
-                    unit=measurement['unit'],
-                    averagingPeriod=""
-                    )
+        mes_id = self.mdao.store(series_id=series_id, value=measurement['value'], date=measurement['date']['utc'])
+        self.assertTrue(mes_id)
+        result = self.mdao.get_all()
+        self.assertEqual(len(result), 1)
 
 
-    print(f'{res1} == {res2}')
-    assert res2 is not None
-    assert res2 == res1
+    def test_insert_all(self):
 
-    result = dao.get_all()
-    assert len(result) == 1
-    dao.drop_table()
+        station, measurement, _ = split_record(json.loads(jseg))
+        station_id = self.smdao.store_from_json(station)
+
+        series_id = self.dao.store(station_id=station_id,
+                        parameter=measurement['parameter'],
+                        unit=measurement['unit'],
+                        averagingPeriod=""
+                        )
+
+        result = self.dao.get_all()
+        self.assertEqual(len(result), 1)
+
+        mes_id = self.mdao.store(series_id=series_id, value=measurement['value'], date=measurement['date']['utc'])
+        self.assertTrue(mes_id)
+        res = self.mdao.get_all()
+        self.assertEqual(len(res), 1)
 
 
-def test_get_forstation():
-    smdao, dao, mdao = __get__daos()
+    @raises(IntegrityError)
+    def test_foreignkey_violation(self):
 
-    with open('./tests/series.ndjson', 'rb') as f:
-        ll = list(get_jsons_from_stream(stream=f, object_name='series.ndjson'))
-        assert len(ll) == 15
+        station, measurement, _ = split_record(json.loads(jseg))
 
-    for rec in ll:
-        _, measurement, _ = split_record(rec)
-        station_id = smdao.store_from_json(rec)
-        series_id = dao.store(station_id=station_id, 
-            parameter = measurement['parameter'],
-            unit=measurement['unit'],
-            averagingPeriod=f"{measurement['averagingPeriod']}")
+        res = self.dao.store(station_id='non-existing name',
+                        parameter=measurement['parameter'],
+                        unit=measurement['unit'],
+                        averagingPeriod=""
+                        )
+        result = self.dao.get_all()
+        assert len(result) == 1
         
-        mes_id = mdao.store(series_id=series_id, value=measurement['value'], date=measurement['date']['utc'])
-    mes = mdao.get_all()
-    assert len(mes) == 15
+        if self.engine.get_engine().name == 'sqlite':
+            raise IntegrityError(statement='SQLLITE', params='does not support', orig='remote key violation',
+                                connection_invalidated=False, code=None)
 
-    only_one = dao.get_all_for_station(station_id="Nisekh")
-    assert len(only_one) == 1
+    def test_multiple_inserts(self):
 
-    more_than_one = dao.get_all_for_station(station_id="Sankt Eriksgatan")
-    assert len(more_than_one) == 3
+        with open('./tests/series.ndjson', 'rb') as f:
+            ll = list(get_jsons_from_stream(stream=f, object_name='series.ndjson'))
+            self.assertEqual(len(ll), 15)
 
-    my_series = dao.get_for_id(series_id=1)
-    assert my_series[2] == 'pm10'
+        for rec in ll:
+            station, measurement, _ = split_record(rec)
+            station_id = self.smdao.store_from_json(rec)
+            series_id = self.dao.store(station_id=station_id, 
+                parameter = measurement['parameter'],
+                unit=measurement['unit'],
+                averagingPeriod=f"{measurement['averagingPeriod']}")
+            
+            mes_id = self.mdao.store(series_id=series_id, value=measurement['value'], date=measurement['date']['utc'])
+        
+        stations = self.smdao.get_all()
+        self.assertEqual(len(stations),4)
 
-    __clean_daos(smdao, dao, mdao)
+        series = self.dao.get_all()
+        self.assertEqual(len(series),6)
+
+        mes = self.mdao.get_all()
+        self.assertEqual(len(mes), 15)
+
+
+    def test_store_unique_series(self):
+
+        asj = json.loads(jseg)
+        id1 = self.smdao.store_from_json(asj)
+        self.assertIsNotNone(id1) 
+
+        station, measurement, _ = split_record(json.loads(jseg))
+
+        res1 = self.dao.store(station_id=id1,
+                        parameter=measurement['parameter'],
+                        unit=measurement['unit'],
+                        averagingPeriod=""
+                        )
+
+        
+        res2 = self.dao.store(station_id=id1,
+                        parameter=measurement['parameter'],
+                        unit=measurement['unit'],
+                        averagingPeriod=""
+                        )
+
+
+        print(f'{res1} == {res2}')
+        self.assertIsNotNone(res2)
+        self.assertIsNotNone(res1)
+        self.assertEqual(res1, res2)
+
+        result = self.dao.get_all()
+        self.assertEqual(1, len(result))
+
+
+    def test_get_forstation(self):
+        with open('./tests/series.ndjson', 'rb') as f:
+            ll = list(get_jsons_from_stream(stream=f, object_name='series.ndjson'))
+            self.assertEqual(15, len(ll))
+
+        for rec in ll:
+            _, measurement, _ = split_record(rec)
+            station_id = self.smdao.store_from_json(rec)
+            series_id = self.dao.store(station_id=station_id, 
+                parameter = measurement['parameter'],
+                unit=measurement['unit'],
+                averagingPeriod=f"{measurement['averagingPeriod']}")
+            
+            mes_id = self.mdao.store(series_id=series_id, value=measurement['value'], date=measurement['date']['utc'])
+        mes = self.mdao.get_all()
+        self.assertEqual(15, len(mes))
+
+        only_one = self.dao.get_all_for_station(station_id="Nisekh")
+
+        self.assertEqual(1, len(only_one))
+        
+        more_than_one = self.dao.get_all_for_station(station_id="Sankt Eriksgatan")
+        
+        self.assertEqual(3, len(more_than_one))
+        my_series = self.dao.get_for_id(series_id=1)
+
+        self.assertEqual(my_series[2],'pm10')
 
